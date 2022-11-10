543abf3ef4aa7dc5d49bbb9a4d4b71b36fd02c94
Iqra Zafar <iqrazafar927@gmail.com>
2022-11-10 23:55:13 +0500

    lab11

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//task3
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByTestId("comp");
+  expect(linkElement).toHaveTextContent("Hello World");
+
 });
